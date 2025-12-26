454afd92d64cc459df427f41f6e24fdba56ba2af
IlyaDestroyer <ilya.mory@yandex.ru>
2025-12-26 10:41:03 +0300

    Update car.py

--- src/car.py
+++ src/car.py
@@ -16,6 +16,6 @@ class Car:
         # Считаем, что расход 8 литров на 100 км
         fuel_burned: int = 8 * (distance_km / 100)
         if self._fuel_in_tank < fuel_burned:
-            pull
+            pass
         self._fuel_in_tank -= fuel_burned
         return self.get_current_fuel_level()
